c1b595774e381f068942f98642124cf0534880e9
M-Zachary <zachary.manning3@education.nsw.gov.au>
2023-10-24 09:46:38 +1100

    suspention v2

---
+++ Assets/car.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class car : MonoBehaviour
+{
+    public float strength;
+    public float damping;
+    public float length;
+}
--- Assets/wheels.cs
+++ Assets/wheels.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class wheels : MonoBehaviour
 {
-    public GameObject body;
-    Rigidbody car;
-    int strength = 100;
-    int damping = 15;
+    GameObject parentGameObject;
+    Rigidbody parentRigidbody;
+
+    GameObject childGameObject;
+
+    float strength;
+    float damping;
+    float length;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        car = body.GetComponent<Rigidbody>();
+        parentGameObject = transform.parent.gameObject;
+        childGameObject = transform.GetChild(0).gameObject;
+
+        car parentScript = parentGameObject.GetComponent<car>();
+
+        strength = parentScript.strength;
+        damping = parentScript.damping;
+        length = parentScript.length;
+        parentRigidbody = parentGameObject.GetComponent<Rigidbody>();
       
     }
 
@@ -21,22 +34,27 @@ public class wheels : MonoBehaviour
     {
         RaycastHit hit;
 
-        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.down), out hit, 1))
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.down), out hit, length))
         {
             Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.down) * hit.distance, Color.red);
+            childGameObject.transform.position = hit.point + new Vector3(0, 0.25f, 0);
 
             Vector3 springDir = transform.up;
-            Vector3 wheelVel = car.GetPointVelocity(transform.position);
-            float offset = 1 - hit.distance;
+            Vector3 wheelVel = parentRigidbody.GetPointVelocity(transform.position);
+            float offset = length - hit.distance;
             float vel = Vector3.Dot(springDir, wheelVel);
             float force = (offset * strength) - (vel * damping);
 
-            car.AddForceAtPosition(springDir * force, transform.position);
+            parentRigidbody.AddForceAtPosition(springDir * force, transform.position);
         }
         else
         {
-            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.down), Color.white);
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.down) * length, Color.white);
+            Ray ray = new Ray(transform.position, transform.TransformDirection(Vector3.down));
+            childGameObject.transform.position = ray.GetPoint(length);
         }
 
+        
+   
     }
 }
